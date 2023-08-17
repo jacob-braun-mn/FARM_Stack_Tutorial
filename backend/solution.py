0024747a9ffd2343f4e54f9b8562fb2f4fd3bbe1
Jake Braun <jakebraun14@gmail.com>
2023-08-17 12:31:57 -0500

    Fix errors and add solution file

--- backend/solution.py
+++ backend/solution.py
@@ -1,21 +1,26 @@
 from fastapi import FastAPI, HTTPException
-from fastapi.middleware.cors import CORSMiddleware # Cross Origin Resource Sharing - allows JS frontend to communicate with backend
+
 from model import Todo
+
 from database import (
     fetch_one_todo,
     fetch_all_todos,
     create_todo,
     update_todo,
-    remove_todo
+    remove_todo,
 )
-# uvicorn main:app --reload
 
+# an HTTP-specific exception class  to generate exception information
 
-# App object
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+]
 
-origins = ['http://localhost:3000']
+# what is a middleware? 
+# software that acts as a bridge between an operating system or database and applications, especially on a network.
 
 app.add_middleware(
     CORSMiddleware,
@@ -25,33 +30,28 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
-
 
 @app.get("/api/todo")
 async def get_todo():
     response = await fetch_all_todos()
     return response
 
-
 @app.get("/api/todo/{title}", response_model=Todo)
 async def get_todo_by_title(title):
     response = await fetch_one_todo(title)
     if response:
         return response
-    raise HTTPException(404, f'There is no Todo item with the title, "{title}"')
-
+    raise HTTPException(404, f"There is no todo with the title {title}")
 
 @app.post("/api/todo/", response_model=Todo)
-async def post_todo(todo:Todo):
-    response = await create_todo(todo.model_dump())
+async def post_todo(todo: Todo):
+    response = await create_todo(todo.dict())
     if response:
         return response
     raise HTTPException(400, "Something went wrong")
-
 
 @app.put("/api/todo/{title}/", response_model=Todo)
 async def put_todo(title: str, desc: str):
@@ -60,11 +60,9 @@ async def put_todo(title: str, desc: str):
         return response
     raise HTTPException(404, f"There is no todo with the title {title}")
 
-
 @app.delete("/api/todo/{title}")
 async def delete_todo(title):
     response = await remove_todo(title)
     if response:
-        return "Successfully deleted todo item!"
-    raise HTTPException(404, f'There is no Todo item with the title, "{title}"')
-
+        return "Successfully deleted todo"
+    raise HTTPException(404, f"There is no todo with the title {title}")
